c3f0b2cad6933cd7c05e0485e1e4933b100bd323
1lxrd <yashnik.dmitriu@gmail.com>
2024-02-07 13:59:06 +0200

    Adding converting script v1.0

---
+++ webp-conv.js
@@ -0,0 +1,20 @@
+dir = '/home/xnelxrd/Documents/'; //variable with path to files
+temp = /(.+\.jpg)|(.+\.jpeg)|(.+\.png)/ //regular for checking if filetype is image, add more if u want to use another type of image
+
+const webp=require('webp-converter');
+const fs = require('fs');
+fs.readdir(dir,(err, files) => { //opening folder with files
+    if (err) throw err;
+
+    for (i in files){ //cycle for finding all files
+        let a = temp.test(files[i]); //checking if filetype is an image (comparing to regular expression)
+        if (a){
+            namef = files[i].split('.'); //receiving name of image, DON'T USE DOT IN FILENAMES
+            const result = webp.cwebp(files[i],`result/${namef[0]}.webp`,"-q 80",logging="-v"); //converting
+            result.then((response) => {
+            console.log(response); //logging
+            });
+        }
+    }
+
+})
