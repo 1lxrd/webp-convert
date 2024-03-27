685be71b7c1fd6fd3aea4c8d3f24c824379e9587
1lxrd <yashnik.dmitriu@gmail.com>
2024-03-27 20:20:21 +0200

    Updated logic, refactored code, added metadata encapsulation, added checking metadata for already optimized image, added jpeg png webp support

--- convert.py
+++ convert.py
@@ -2,32 +2,67 @@ from ftplib import FTP
 from Naked.toolshed.shell import execute_js, muterun_js
 from pathlib import Path
 import re
+import csv
+import os
+import json
 
+
+def add_meta(image):
+    with open(image, 'a+b') as f:
+        f.write(json.dumps({'Description': 'Optimized'}).encode('utf-8'))
+
+
+def read_meta(image):
+    with open(image, 'rb') as f:
+        data = str(f.read())
+    meta = re.search(r'xff.*({.*})\'\Z', data)
+    return meta
+
+
+def download(f):
+    match = re.search(temp, f)
+    if match:
+        with open(f'{f}', 'wb') as file:
+            ftp.retrbinary('RETR ' + f, file.write)
+    print(f'{f} downloaded')
+
+
+def check_if_optimized(f):
+    meta = read_meta(f)
+    if not meta:
+        muterun_js('webp-conv.js', arguments=f'q=4')
+        print(f'{f} optimized')
+        add_meta(f)
+
+
+def upload(p, f, ext):
+    for i in p.rglob(ext):
+        with open(f'{f}', 'rb') as file:
+            ftp.storbinary(f'STOR {f}', file)
+        print(f'{f} uploaded')
+        os.remove(f)
+
+
+with open('config.csv') as f:
+    reader = csv.reader(f)
+    scan = list(reader)
+    config = scan[1]
 
 temp = r'(.+\.jpg)|(.+\.jpeg)|(.+\.png)|(.+\.JPG)|(.+\.JPEG)'
 
-ftp = FTP('ftp.intel.com')
-ftp.login()
-a = ftp.retrlines('LIST')
-print(a)
-ftp.cwd('images')
+ftp = FTP(config[0])
+ftp.login(user=config[1], passwd=config[2])
+ftp.retrlines('LIST')
+ftp.cwd(config[3])
 ftp.retrlines('LIST')
 a = ftp.nlst()
+
 for i in a:
-    match = re.search(temp, i)
-    if match:
-        with open(f'{i}', 'wb') as file:
-            ftp.retrbinary('RETR ' + i, file.write)
+    download(i)
+    check_if_optimized(i)
 
-response = muterun_js('webp-conv.js')
-print(response.stdout)
-
-p = Path("./result")
-for i in p.rglob('*.webp'):
-    print(i)
-    # with open(f'{i}', 'rb') as file:
-    #     ftp.storbinary(f'STOR {i}', file)
-
-
-
-
+    p = Path("./")
+    upload(p, i, '*.jpg')
+    upload(p, i, '*.png')
+    upload(p, i, '*.jpeg')
+    upload(p, i, '*.webp')
--- webp-conv.js
+++ webp-conv.js
@@ -1,22 +1,21 @@
 dir = './'; //variable with path to files
 
-//!!! BEFORE USING CREATE RESULT FOLDER in this directory !!!
-
 temp = /(.+\.jpg)|(.+\.jpeg)|(.+\.png)|(.+\.JPG)|(.+\.JPEG)/ //regular for checking if filetype is image, add more if u want to use another type of image
 
 const webp=require('webp-converter');
 const fs = require('fs');
+
 fs.readdir(dir,(err, files) => { //opening folder with files
     if (err) throw err;
 
     for (i in files){ //cycle for finding all files
         let a = temp.test(files[i]); //checking if filetype is an image (comparing to regular expression)
         if (a){
-            namef = files[i].split('.'); //receiving name of image, DON'T USE DOT IN FILENAMES
-            const result = webp.cwebp(files[i],`result/${namef[0]}.webp`,"-q 80",logging="-v"); //converting
+            const result = webp.cwebp(files[i],files[i],'-q 70',logging="-v"); //converting
             result.then((response) => {
             console.log(response); //logging
             });
+
         }
     }
 
